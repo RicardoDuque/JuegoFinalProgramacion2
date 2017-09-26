ecc727df9dafa41599eaa86cf198521d252a3ae5
RicardoDuque <rdg1421@gmail.com>
2017-09-26 00:12:44 -0500

    Juego_Finalv1.0
    Juego básico, movimiento de jugador y plataformas

---
+++ Juego_FinalV1/Assets/MovementNoForces.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementNoForces : MonoBehaviour {
+
+    public float speed = 5; //Modificador velocidad 
+    public bool grounded;//Saber si estamos tocando el suelo
+    public float jumpPower = 6.5f; //Variable para el salto
+    private Animator anim; //Importar animaciones
+    private Rigidbody2D rb2d; //Importar RigidBody
+    private bool jump; //Para detectar botón
+    
+
+    // Use this for initialization
+    void Start () {
+		
+		rb2d = GetComponent<Rigidbody2D> (); //Fuerza RigidBody // Hace referencia al RigidBody del objeto Player	
+		anim = GetComponent<Animator>();//Importar animaciones del Player
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate(){
+		
+		//float move = Input.GetAxis ("Horizontal");
+		bool leftMove = Input.GetKey (KeyCode.LeftArrow);
+		bool rightMove = Input.GetKey (KeyCode.RightArrow);
+
+		//Movimiento jugador
+		if (leftMove) {
+			rb2d.velocity = new Vector2 (-speed, rb2d.velocity.y);
+			transform.localScale = new Vector3 (-1f, 1f, 1f);	//Voltea el sprite
+		}
+		if (rightMove) {
+			rb2d.velocity = new Vector2 (speed, rb2d.velocity.y);
+			transform.localScale = new Vector3 (1f, 1f, 1f);	//Voltea el sprite
+		}
+
+		/*
+		//Detener jugador al soltar tecla		
+		if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow))
+			rb2d.velocity = new Vector2 (0, rb2d.velocity.y);
+		*/
+		//Jugador no se deslice
+		Vector3 fixedVelocity = rb2d.velocity;
+		fixedVelocity.x *= 0.75f; 
+		if (grounded)
+			rb2d.velocity = fixedVelocity;
+		else
+			rb2d.velocity = fixedVelocity;
+
+		//Para cambbio animaciones
+		anim.SetFloat ("Speed", Mathf.Abs (rb2d.velocity.x)); //Mathf.Abs: Busca el valor absoluto o la distancia que hay entre un punto
+		//y otro punto, independientemente de si es positivo o negativo, es la dist. 
+		anim.SetBool ("Grounded", grounded);//Saber si estamos tocando el suelo
+
+
+		//Detectar si estamos en el suelo y poder saltar
+		if (Input.GetKeyDown (KeyCode.UpArrow) && grounded)
+			jump = true;
+		if (jump) {
+			rb2d.velocity = new Vector2 (rb2d.velocity.x, 0); //Si salta dos veces, que a la segunda vez no tome impulso y de un salto
+															  //grande, sino normal; que no se agregue la fuerza y se sumen
+			rb2d.AddForce (Vector2.up * jumpPower, ForceMode2D.Impulse);
+			jump = false;
+			}
+
+		}
+	void OnBecameInvisible(){ //Detecta cuando desaparecemos de la escena
+
+        //Vector3 punto = (mainCamera.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, mainCamera.nearClipPlane)));
+        //rb2d.transform.position = new Vector3(0, -4, 0);
+        //Debug.Log(mainCamera.gameObject.transform.position.x);
+        //   Rect(Screen.width / 2, Screen.height);
+        float centerPos = Camera.current.transform.position.x;
+
+        transform.position = new Vector3(centerPos, -4, 0);
+
+    }
+
+}
+
---
+++ Juego_FinalV1/Assets/PlataformaMueve.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlataformaMueve : MonoBehaviour {
+
+	public Transform target;
+	public float speed;
+	private Vector3 start, end; //Determinar posición de inicio y final para que cambie cuando llegue al final
+
+
+	// Use this for initialization
+	void Start () {
+		if (target != null) {
+			target.parent = null; //El target ya va a ser independiente y no se moverá con la plataforma, se queda en la misma posición
+			start = transform.position;  //Posición inciial de la plaatforma
+			end = target.position; //Posición final de la plataforma, cuando llega al target
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate(){
+		if (target != null){
+			float fixedSpeed = speed * Time.deltaTime; //Velocidad multiplicada por el deltaTime(segs)
+			transform.position = Vector3.MoveTowards (transform.position, target.position, fixedSpeed); //Mueve entre transformPosition(actual)
+																								   //y la posición del target con X velocidad
+		}
+
+		if (transform.position == target.position) { //Si la posición de la plataforma es igual a la posición del target
+
+			//target.position = (target.position == start) ? end : start)://Si targetPosition es igual a la start entonces, target position
+			//tendrá valor de end, si no tendrá valor de start
+			if (target.position == start)
+				target.position = end;
+			else
+				target.position = start;
+
+		}
+
+
+	}
+}
